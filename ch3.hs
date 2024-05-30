412b6704f4ac9c729f2764e760397a7844e1aa50
Justin <siegfrij@email.sc.edu>
2024-05-30 15:37:38 -0400

    Chap 3 & 4 Done

--- ch3.hs
+++ ch3.hs
@@ -1,15 +1,28 @@
+add :: Num a => (a, a) -> a
 add (x,y) = x+y
 
+add' :: Num a => a -> a -> a
 add' x y = x+y
 
+second :: [a] -> a
 second xs = head (tail xs)
 
+-- !! is another way to index a list
+second' :: [a] -> a
+second' = (!!1)
+
+swap :: (b, a) -> (a, b)
 swap (x,y) = (y,x)
 
+pair :: a -> b -> (a, b)
 pair x y = (x,y)
 
+double :: Num a => a -> a
 double x = x*2
 
+palindrome :: Eq a => [a] -> Bool
 palindrome xs = reverse xs == xs
 
+twice :: (t -> t) -> t -> t
 twice f x = f (f x) 
+
--- ch4_base.hs
+++ ch4_base.hs
@@ -1,36 +1,73 @@
+import Distribution.Simple.Utils (xargs)
 --null'
---null' :: [a] -> Bool
+null' :: [a] -> Bool
+null' xs = if length xs == 0 then True else False
 
 --null' cond
 
 
+
 --null'' pattern
+null'' :: [a] -> Bool
+null'' [] = True
+null'' _  = False
 
 --null''' guarded
+null''' xs 
+   | xs == [] = True
+   | otherwise = False
 
 
 --safetail
 --safetail cond
---safetail :: [a] -> [a]
+safetail :: [a] -> [a]
+safetail xs =  if null xs then [] else tail xs
+
 
 --safetail pattern
+safetail' :: [a] -> [a]
+safetail' [] = []
+safetail' xs = tail xs
+
 
 --safetail guarded
+safetail'' :: [a] -> [a]
+safetail'' xs
+   | null xs = []
+   | otherwise = tail xs
 
---(||) :: Bool -> Bool -> Bool
+
 --explicit
- --match both arguments
+{-
+(|||) :: Bool -> Bool -> Bool
+False ||| False = False
+False ||| True  = True
+True  ||| False = True
+True  ||| True  = True
+-}
+
+--match both arguments
+(|||) :: Bool -> Bool -> Bool
+False ||| False = False
+_     ||| _     = True
 
 --match first arg only
+(||||) :: Bool -> Bool -> Bool
+False |||| x = x
+_     |||| _ = True
 
 
 -- && with cond
 {- True && True = True
    _    && _    = False -}
 --(&&) :: Bool -> Bool -> Bool
+(&&&) :: Bool -> Bool -> Bool
+(&&&) x y = if x then if y then True else False else False
 
 
 --above, again, with slightly different logic
+(&&&&) :: Bool -> Bool -> Bool
+x &&&& y = if x then y else False
 
 
 
